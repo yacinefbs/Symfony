235e7d6c987ff54dd85ca47b3162d73c527c9549
yacinefbs <naouiyassine009@gmail.com>
2017-03-12 14:26:44 +0000

    Les services, théorie et création

---
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -0,0 +1,27 @@
+<?php
+namespace OC\PlatformBundle\Antispam;
+
+class OCAntispam
+{
+    /**
+     * Vérifie si le texte est un spam ou non
+     *
+     * @param string $text
+     * @return bool
+     */
+    private $mailer;
+    private $locale;
+    private $minLength;
+
+    public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
+    {
+        $this->mailer    = $mailer;
+        $this->locale    = $locale;
+        $this->minLength = (int) $minLength;
+    }
+
+    public function isSpam($text)
+    {
+        return strlen($text) < $this->minLength;
+    }
+}
